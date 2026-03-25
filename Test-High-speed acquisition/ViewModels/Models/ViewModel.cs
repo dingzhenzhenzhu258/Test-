28ce60f4085af78a8ab0a60eeb463f1ad76cdbc9
LJH <LJH@DESKTOP-75RJLKC>
2026-03-25 16:06:01 +0800

    完善

--- Test-High-speed acquisition/ViewModels/Models/ViewModel.cs
+++ Test-High-speed acquisition/ViewModels/Models/ViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,8 +13,39 @@ namespace Test_High_speed_acquisition.ViewModels.Models
     /// <summary>
     /// 视图模型基类，封装页面导航生命周期方法。
     /// </summary>
-    public abstract class ViewModel : ObservableObject, INavigationAware
+    public abstract class ViewModel : ObservableValidator, INavigationAware
     {
+        // 添加序列化/反序列化方法
+        /// <summary>
+        /// 序列化
+        /// </summary>
+        /// <returns></returns>
+        public virtual JObject Serialize()
+        {
+            // 只序列化标记了[JsonProperty]的属性
+            return JObject.FromObject(this);
+        }
+
+        /// <summary>
+        /// 将传入的 JObject 数据反序列化，填充到当前对象实例中
+        /// </summary>
+        /// <param name="data"></param>
+        public virtual void Deserialize(JObject data)
+        {
+            JsonConvert.PopulateObject(data.ToString(), this);
+        }
+
+        // 添加虚拟方法用于子类自定义序列化
+        /// <summary>
+        /// 在对象被序列化之前调用
+        /// </summary>
+        public virtual void BeforeSerialize() { }
+
+        /// <summary>
+        /// 在对象被反序列化之后调用
+        /// </summary>
+        public virtual void AfterDeserialize() { }
+
         /// <inheritdoc />
         public virtual Task OnNavigatedToAsync()
         {
